32195692ef14466a84400d261c85ee6c433facf9
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2017-09-16 18:36:00 -0700

    display points based on an array of lat/longs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import ArcgisMap from './components/arcgis_map'
+import ArcgisScene from './components/arcgis_map'
 
 class App extends Component {
   constructor(props) {
     super(props)
 
   }
-  
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Disaster Relief</h2>
         </div>
-        <ArcgisMap />
+        <ArcgisScene />
       </div>
     );
   }
--- src/components/arcgis_map.js
+++ src/components/arcgis_map.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Map, WebMap, WebScene, Popup, Scene, Layers, Geometry, Graphic, Symbols } from 'react-arcgis';
+import { Scene, Layers, Geometry, Graphic, Symbols } from 'react-arcgis';
 
-class ArcgisMap extends Component {
+class ArcgisScene extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -11,63 +11,62 @@ class ArcgisMap extends Component {
       viewProperties: {
         center: [-122.431297, 37.7749], // florida keys: [-80.447281, 25.086515]
         scale: 50000
-      }
+      },
+      symbolProperties: {
+          color: [226, 119, 40],
+          outline: {
+              color: [255, 255, 255],
+              width: 2
+          }
+      },
+      points: [
+          {
+            lat: 37.7749,
+            lng: -122.431297
+          },
+          {
+            lat: 37.781975,
+            lng: -122.449335
+          },
+          {
+            lat: 37.778393,
+            lng: -122.410646
+          }
+      ]
     }
   }
 
+
+
   render() {
+    const points = this.state.points.map( (point) => {
+      return (
+        <Graphic key={point.lat + point.lng}>
+          <Symbols.SimpleMarkerSymbol
+              symbolProperties={this.state.symbolProperties}
+          />
+          <Geometry.Point
+              geometryProperties={{
+                  latitude: point.lat,
+                  longitude: point.lng
+              }}
+          />
+        </Graphic>
+      )
+    })
+
     return (
       <Scene
         style={{ width: '100vw', height: '100vh' }}
         mapProperties={this.state.mapProperties}
         viewProperties={this.state.viewProperties}
       >
-        <Popup
-            popupProperties={{
-                content: 'This is a random popup that I made.',
-                location: [-122.431297, 37.7749],
-                title: 'My Popup'
-            }}
-        />
         <Layers.GraphicsLayer>
-          <Graphic>
-              <Symbols.SimpleMarkerSymbol
-                  symbolProperties={{
-                      color: [226, 119, 40],
-                      outline: {
-                          color: [255, 255, 255],
-                          width: 2
-                      }
-                  }}
-              />
-              <Geometry.Point
-                  geometryProperties={{
-                      latitude: 37.7749,
-                      longitude: -122.431297
-                  }}
-              />
-          </Graphic>
-          <Graphic>
-              <Symbols.SimpleMarkerSymbol
-                  symbolProperties={{
-                      color: [226, 119, 40],
-                      outline: {
-                          color: [255, 255, 255],
-                          width: 2
-                      }
-                  }}
-              />
-              <Geometry.Point
-                  geometryProperties={{
-                      latitude: 37.781975,
-                      longitude: -122.449335
-                  }}
-              />
-          </Graphic>
+          { points }
         </Layers.GraphicsLayer>
       </Scene>
     )
   }
 }
 
-export default ArcgisMap;
+export default ArcgisScene;
